a7391e0b109019fbbeeb3b414fecaa89d9a98ef0
Pineda <juansejopineda@gmail.com>
2025-05-06 19:14:05 -0500

    Primer comit

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+namespace SupermarketWEB.Data
+{
+    public class SupermarketContext: DbContext
+    {
+        public SupermarketContext(DbContextOptions<SupermarketContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public category category { get; set; }
+
+    }
+}
