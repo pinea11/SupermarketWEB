cc6e0fca2151f8f87cd321a034d60dd116e435da
Pineda <juansejopineda@gmail.com>
2025-05-06 21:33:53 -0500

    Segundo commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+
 namespace SupermarketWEB
 {
     public class Program
@@ -8,6 +11,12 @@ namespace SupermarketWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+
+            //Agregando el contexto SupermarketContext a la aplicacion
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
+                );
 
             var app = builder.Build();
 
