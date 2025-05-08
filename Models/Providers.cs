77e4fcc552c95f899bfb512b6f01c0fa9df98cdb
Pineda <juansejopineda@gmail.com>
2025-05-07 21:56:23 -0500

    provider

---
+++ Models/Providers.cs
@@ -0,0 +1,11 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; } // Llave primaria
+        public string Name { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+    }
+}
+
